c01a7d07e3de933ff98cc770c0f24de4596ccde0
Barbapapazes <e.soubiran25@gmail.com>
2025-11-21 15:22:18 +0100

    chore: add version to main package.json

--- bumpp.config.ts
+++ bumpp.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'bumpp'
 export default defineConfig({
   sign: true,
   files: [
+    'package.json',
     'packages/vitepress-plugin-og/package.json',
     'packages/rspress-plugin-og/package.json',
   ],
